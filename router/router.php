7263ad5a142ba312c94b46eb0a929a31225a9418
Hayden Bradfield <me@haydenbradfield.com>
2026-05-30 23:34:09 -0400

    wip

--- router/router.php
+++ router/router.php
@@ -4,11 +4,26 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Views\Twig;
 
+require __DIR__ . "/util/dbo.php";
+
 function use_router($app) {
 
     $app->get('/', function (Request $request, Response $response) {
         $view = Twig::fromRequest($request);
-        return $view->render($response, 'home.page.twig');
+
+        $conn = createDBInstance();
+
+        $result = $conn->query("SELECT * FROM articles WHERE is_featured = 1 ORDER BY published_date DESC");
+
+        $featured_articles = [];
+
+        if($result->num_rows > 0){
+            while($row = $result->fetch_assoc()){
+                array_push($featured_articles, $row);
+            }
+        }
+
+        return $view->render($response, 'home.page.twig', ["featured_articles" => $featured_articles]);
     });
 
     $app->get('/problems', function (Request $request, Response $response) {
