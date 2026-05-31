a33cdf4630a16615b20b848a2cba089286c29b02
Hayden Bradfield <me@haydenbradfield.com>
2026-05-31 14:34:47 -0400

    wip

---
+++ .resources/.sql_files/.articles/tn_lacks_enhanced_charges_assaults_ems_fire.sql
@@ -0,0 +1,23 @@
+INSERT INTO articles (
+    article_title,
+    article_author,
+    published_date,
+    updated_date,
+    content,
+    tile_image_uri,
+    tile_image_alt,
+    is_featured,
+    is_visible,
+    slug
+) VALUES (
+    '[ARCHIVED] Unlike most states, Tennessee Lacks Enhanced Charges For Assaults Against EMS Personnel',
+    'Hayden Bradfield',
+    '2025-05-27',
+    NULL,
+    '<p>Tennessee is missing its mark once again when it pertains to protecting its Emergency Medical Technicians and Paramedics. Recently,legislation was passed to enhance charges and penalties on assaults towards law enforcement personnel through the "Back the Blue Act." Other public safety workers, such as EMS personnel and firefighters, were not considered as part of this.</p><p>EMS personnel face growing threats of assaults and on-the-job injuries. Sadly, there is not much attention or recognition here in Tennessee to address these issues. Assault or battery against ANY public safety worker should be a felony!</p><p>Below is a list of states and their respective laws pertaining to assaults against EMS. The vast majority of these states make assaults against EMTs and Paramedics an aggrivated offense (felony). I only counted 10 states that either didn''t have enhanced charges and/or penalties or where the information was not in reach. Sadly, Tennessee was one of those 10.</p><table class="table"><tbody><tr><td>Alabama</td><td>AL Code 13A-6-21</td></tr><tr><td>Alaska</td><td>[[Misdemeanor]]</td></tr><tr><td>Arizona</td><td>AZ Statute 13-1204</td></tr><tr><td>Arkansas</td><td>AR Code 12-13-202</td></tr><tr><td>California</td><td>[[Misdemeanor]]</td></tr><tr><td>Colorado</td><td>Colorado Revised Statutes 18-3-203</td></tr><tr><td>Connecticut</td><td>CT Revised Statute 53a-167c</td></tr><tr><td>Deleware</td><td>DE Code 11 613</td></tr><tr><td>Florida</td><td>FL Statute 784.07</td></tr><tr><td>Georgia</td><td>O.C.G.A 16-10-24.2</td></tr><tr><td>Hawaii</td><td>HI Revised Statutes 707-711</td></tr><tr><td>Idaho</td><td>ID Code 18-915</td></tr><tr><td>Illinois</td><td>720 IL Statutes 5/12-2 & 5/12-4</td></tr><tr><td>Indiana</td><td>IN Code 35-42-2-1</td></tr><tr><td>Iowa</td><td>IA Code 708.3A</td></tr><tr><td>Kansas</td><td>[[N/A]]</td></tr><tr><td>Kentucky</td><td>KY Revised Statute 14:322</td></tr><tr><td>Louisiana</td><td>LA Revised Statutes 14 34.8</td></tr><tr><td>Maine</td><td>Maine 752-C</td></tr><tr><td>Maryland</td><td>CR 3-203</td></tr><tr><td>Massachusetts</td><td>MA Ch. 265 13i</td></tr><tr><td>Michigan</td><td>MI 750.81d</td></tr><tr><td>Minnesota</td><td>MN 609.2231</td></tr><tr><td>Mississippi</td><td>MS Code 97-3-7</td></tr><tr><td>Missouri</td><td>MO Revised Statute 565.082</td></tr><tr><td>Montana</td><td>[[N/A]]</td></tr><tr><td>Nebraska</td><td>NE Revised Statute 28-930, 28-931</td></tr><tr><td>Nevada</td><td>NV Revised Statute 200.471</td></tr><tr><td>New Hampshire</td><td>[[N/A]]</td></tr><tr><td>New Mexico</td><td>[[MISDEMEANOR]] Defines misdemeanor charges for simple assault or battery against EMT</td></tr><tr><td>New Jersey</td><td>NJ Revised Statutes 2C:12-1</td></tr><tr><td>New York</td><td>NY Penal Code 120.05, 120.08</td></tr><tr><td>North Carolina</td><td>NC 14-34.6</td></tr><tr><td>North Dakota</td><td>ND 12.1-17-01</td></tr><tr><td>Ohio</td><td>Ohio 2903.13</td></tr><tr><td>Oklahoma</td><td>OK 21-650.4</td></tr><tr><td>Oregon</td><td>OR Revised Statute 163.165</td></tr><tr><td>Pennsylvania</td><td>PA Title 18 2702</td></tr><tr><td>Rhode Island</td><td>RI 16-3-635</td></tr><tr><td>South Carolina</td><td>[[Unknown]]</td></tr><tr><td>Tennessee</td><td>[[Misdemeanor]]</td></tr><tr><td>Texas</td><td>TX Penal Code 22.01</td></tr><tr><td>Utah</td><td>[[Misdemeanor]]</td></tr><tr><td>Vermont</td><td>VT Statute Title 13 1028</td></tr><tr><td>Virginia</td><td>VA Code 18.2-51.1</td></tr><tr><td>Washington</td><td>WA Revised Code 9A.36.031</td></tr><tr><td>West Virginia</td><td>WV Code 61-2-10b</td></tr><tr><td>Wisconsin</td><td>WI Statute 940.20</td></tr><tr><td>Wyoming</td><td>[[N/A]]</td></tr></tbody></table>',
+    '/assets/img/jail_image.webp',
+    'Image of walking down jailhouse corridor',
+    0,
+    1,
+    'tn_lacks_enhanced_charges_assaults_ems_fire'
+);
