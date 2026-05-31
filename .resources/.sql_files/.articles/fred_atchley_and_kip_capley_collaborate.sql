69e444125c5298a451fb0870f5c85710d132f039
Hayden Bradfield <me@haydenbradfield.com>
2026-05-31 13:53:15 -0400

    wip

---
+++ .resources/.sql_files/.articles/fred_atchley_and_kip_capley_collaborate.sql
@@ -0,0 +1,23 @@
+INSERT INTO articles (
+    article_title,
+    article_author,
+    published_date,
+    updated_date,
+    content,
+    tile_image_uri,
+    tile_image_alt,
+    is_featured,
+    is_visible,
+    slug
+) VALUES (
+    'Rep. Fred Atchley and Rep. Kip Capley Collaborate to Bring Enhanced Charges For Assaulting Firefighters, EMS',
+    'Hayden Bradfield',
+    '2025-11-29',
+    NULL,
+    '<p>Two representatives in the Tennessee House are collaborating to introduce legislation this upcoming January to bring enhanced felony charges for assaulting firefighters and Emergency Medical Services (EMS) personnel, Jan Wright with Rep. Fred Atchley''s office has confirmed. Upon further discussion with Wright,Rep. Kip Capley (R-Summertown) will be sponsoring the legislation.</p><p>After numerous calls to various legislators urging them to enact this much-needed law, the efforts have finally materialized. As mentioned in <a href="/articles/tn_lacks_enhanced_charges_assaults_ems_fire">another article</a>Tennessee is one of only few states that did not have felony charges for assaults on firefighters and EMS personnel. Kip Capley''s Back the Blue Act made assaults against law enforcement officers a felony, but other first responders were excluded from this legislation. This new legislation seeks to patch these voids.</p><p>You can find my appearence on WBIR discussing this vital issue:</p><p><iframe width="560" height="315" style="max-width:100%;max-height:auto;" src="https://www.youtube.com/embed/tsPtSeG7-cE?si=yvhC8BJgbZrQMJiB" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe></p>',
+    '/assets/img/atchley_capley.webp',
+    'Collage photo of Fred Atchley and Kip Capley',
+    0,
+    1,
+    'fred_atchley_and_kip_capley_collaborate'
+);
